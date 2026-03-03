4af86cf86020589be5e197296c4fa282f9b096fa
stiproot <stipcich.simon@gmail.com>
2026-03-03 11:34:49 +0200

    fix: lspconfig.

--- lua/config/lspconfig.lua
+++ lua/config/lspconfig.lua
@@ -1,8 +1,10 @@
-local lspconfig = require "lspconfig"
-
 local pid = vim.fn.getpid()
 local omnisharp_bin = "/Users/simon.stipcich/code/omnisharp-osx-arm64-net6.0/OmniSharp"
-lspconfig.omnisharp.setup{
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) }
-    -- Additional configuration can be added here
+
+vim.lsp.config.omnisharp = {
+    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) },
+    filetypes = { 'cs', 'vb' },
+    root_dir = vim.fs.dirname(vim.fs.find({ '*.sln', '*.csproj', 'omnisharp.json', 'function.json' }, { upward = true })[1]),
 }
+
+vim.lsp.enable('omnisharp')
